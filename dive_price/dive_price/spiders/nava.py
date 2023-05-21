d37cc3f09b4cee604f0d55b7ea8e0052e192e4fc
Tim Kemeling <timkemeling@gmail.com>
2023-05-21 16:30:51 +0700

    added more scrapers and added to db

---
+++ dive_price/dive_price/spiders/nava.py
@@ -0,0 +1,41 @@
+import scrapy
+from scrapy.http.response import urljoin
+from datetime import datetime
+from dive_price.items import DivePriceItem
+from scrapy.loader import ItemLoader
+
+
+
+class ScubaBirdsSpider(scrapy.Spider):
+    name = "Nava"
+    allowed_domains = ["https://www.navascuba.com/"]
+    start_urls = ["https://www.navascuba.com/diving-level/beginner/",
+                  "https://www.navascuba.com/diving-level/certified-diver/",
+                  "https://www.navascuba.com/diving-level/cont-ed/"]
+
+    def parse(self, response):
+        
+        n = datetime.now()
+        now = n.strftime("%m/%d/%Y")
+        school = "Nava"
+        location = "Koh Tao"
+        agency = "PADI"
+
+        # courses contains all cards for each course. Scrape from here 
+        courses = response.css('a.woocommerce-LoopProduct-link.woocommerce-loop-product__link')
+        for course in courses:
+            url = urljoin('https://www.scubabirds.com', course.css('a::attr(href)').get())
+            l = ItemLoader(item = DivePriceItem(), selector=course)
+
+            price_line = course.css('span.price > span.amount > bdi::text').get()
+            price = price_line.replace('.00', '')
+
+            l.add_css('name', 'h2::text')
+            l.add_value('price', price)
+            l.add_css('course_Link', 'a::attr(href)')
+            l.add_value('agency', agency)
+            l.add_value('school', school)
+            l.add_value('timestamp', now)
+            l.add_value('location', location)
+
+            yield l.load_item()
---
+++ dive_price/dive_price/spiders/new_heaven.py
@@ -0,0 +1,54 @@
+import re
+import scrapy
+from datetime import datetime
+from scrapy.http.response import urljoin
+from dive_price.items import DivePriceItem
+from scrapy.loader import ItemLoader
+
+class ScubaShackSpider(scrapy.Spider):
+    name = "new_heaven"
+    start_urls = ["https://newheavendiveschool.com/"]
+    links_list = []
+
+    def parse(self, response):
+        # Take links in menu and make list of links
+        links_drop = response.css('ul.uk-navbar-nav > li')[0]
+        links_menu = links_drop.css('ul.uk-nav > li')
+
+
+        for link in links_menu:
+            l = link.css('a::attr(href)').get()
+            full = urljoin('https://www.newheavendiveschool.com', l)
+            self.links_list.append(full)
+
+        # Follow links and extract data
+        for link in self.links_list:
+            yield scrapy.Request(link, callback=self.parse_page)
+
+
+    def parse_page(self, response):
+
+        n = datetime.now()
+        now = n.strftime("%m/%d/%Y")
+        school = "New Heaven"
+        location = "Koh Tao"
+        agency = "SSI"
+        url = urljoin('https://www.newheavendiveschool.com', response.request.url)
+
+
+        # courses contains all cards for each course. Scrape from here 
+        course = response.css('body')
+        l = ItemLoader(item = DivePriceItem(), selector=course)
+
+        price_line = course.css('div.uk-text-lead.uk-margin.uk-text-center > p::text').get()
+        price = re.search(r'(\d*\d+)', price_line.lower())
+
+        l.add_css('name', 'h1::text')
+        l.add_value('price', price.group(1))
+        l.add_value('course_Link', url)
+        l.add_value('agency', agency)
+        l.add_value('school', school)
+        l.add_value('timestamp', now)
+        l.add_value('location', location)
+
+        yield l.load_item() 
