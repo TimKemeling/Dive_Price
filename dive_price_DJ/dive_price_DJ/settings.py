b73536bd50d73df8e711c67660f0a507bcd747c4
Tim Kemeling <timkemeling@gmail.com>
2023-05-27 10:13:23 +0700

    fixed imports

--- dive_price_DJ/dive_price_DJ/settings.py
+++ dive_price_DJ/dive_price_DJ/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import config
 from pathlib import Path
+from .config import DJ_secret_key, email_backend_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.DJ_secret_key
+SECRET_KEY = DJ_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,7 +137,7 @@ if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = 'timkemeling@gmail.com'
-    EMAIL_HOST_PASSWORD = config.email_backend_key
+    EMAIL_HOST_PASSWORD = email_backend_key
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     EMAIL_USE_SSL = False
